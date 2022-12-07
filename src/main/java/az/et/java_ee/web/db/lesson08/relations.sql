5d207c5e716650b45a9b55b08b7b278e1a2d870b
Ali Hashimov <ali.hashimovv@gmail.com>
2022-12-07 16:50:09 +0400

    lesson08 code added

--- src/main/java/az/et/java_ee/web/db/lesson08/relations.sql
+++ src/main/java/az/et/java_ee/web/db/lesson08/relations.sql
@@ -22,21 +22,57 @@ from capitals;
 insert into capitals (name, country_id)
 values ('Baku', 1);
 ;
-select * from countries where id=(select country_id from capitals);
-select * from countries as coun inner join capitals c on coun.id = c.country_id join capitals as cap on coun.id = cap.country_id;
-create table author(
-                       id serial primary key ,
-                       name text
+select *
+from countries
+where id = (select country_id from capitals);
+select *
+from countries as coun
+         inner join capitals c on coun.id = c.country_id
+         join capitals as cap on coun.id = cap.country_id;
+create table author
+(
+    id   serial primary key,
+    name text
 );
 
-create table article(
-                        id serial primary key ,
-                        title text not null ,
-                        content text not null ,
-                        author_id int not null ,
-                        constraint fk_author foreign key(author_id) references author(id)
+create table article
+(
+    id        serial primary key,
+    title     text not null,
+    content   text not null,
+    author_id int  not null,
+    constraint fk_author foreign key (author_id) references author (id)
 
 );
-select * from author;
-select * from article;
+drop table article;
+create table article
+(
+    id    serial primary key,
+    title TEXT
+);
+create table tag
+(
+    id        serial primary key,
+    tag_value TEXT
+);
+select *
+from author;
+select *
+from article;
+CREATE table article_tag_rel
+(
+    id         serial
+        PRIMARY KEY,
+    article_id INT,
+    tag_id     INT,
+    constraint fk_article foreign key (article_id) references article (id),
+    constraint fk_tag foreign key (tag_id) references tag (id)
+);
+SELECT *
+FROM article;
+SELECT *
+FROM tag;
+SELECT *
+FROM article_tag_rel;
+
 
